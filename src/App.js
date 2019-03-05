ad71f0d823aeb8921436fcd8f5209810b36bf406
Megha H U <megha_h@ETVMM119785.local>
2019-03-05 19:11:19 +0530

    mobile app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
+import Details from './Components/Details';
+import Cart from './Components/Cart';
+import Default from './Components/Default';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar/>
+        <Switch>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route component={Default}></Route>   
+        </Switch>
+      </React.Fragment>
     );
   }
 }
---
+++ src/data.js
@@ -0,0 +1,103 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title:"Google Pixel - Black",
+        img:"img/product-1.jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title:"Samsung-S7",
+        img:"img/product-2.jpg",
+        price: 10,
+        company: "SAMSUNG",
+        info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 1,
+        title:"Google Pixel - Black",
+        img:"img/product-1.jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 1,
+        title:"Redmi",
+        img:"img/product-1.jpg",
+        price: 10,
+        company: "REDMI",
+        info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 1,
+        title:"Google Pixel - Black",
+        img:"img/product-1.jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title:"Samsung-S7",
+        img:"img/product-2.jpg",
+        price: 10,
+        company: "SAMSUNG",
+        info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 1,
+        title:"Google Pixel - Black",
+        img:"img/product-1.jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 1,
+        title:"Redmi",
+        img:"img/product-1.jpg",
+        price: 10,
+        company: "REDMI",
+        info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+];
+
+export const detailProduct = {
+    id: 1,
+    title:"Google Pixel - Black",
+    img:"img/product-1.jpg",
+    price: 10,
+    company: "GOOGLE",
+    info: "The Google Pixel 3 XL is powered by 2.5GHz octa-core (4x2.5GHz + 4x1.6GHz) Snapdragon 845 processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage cannot be expanded. As far as the cameras are concerned, the Google Pixel 3 XL packs a 12.2-megapixel (f/1.8, 1.4-micron) primary camera on the rear and a 8-megapixel front shooter for selfies",
+    inCart: false,
+    count: 0,
+    total: 0
+}
